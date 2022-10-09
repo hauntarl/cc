5f2355d424d0b1bb99c10e4309d179d0fe4c9842
hauntarl <sameer.mungole@gmail.com>
2022-10-09 14:54:24 -0700

    cses problem #13

--- introductory_problems/creating_strings.py
+++ introductory_problems/creating_strings.py
@@ -2,10 +2,10 @@ from util import timeit
 
 
 @timeit
-def creating_strings_inplace(inp: list[str], beg: int, func) -> None:
+def creating_strings(inp: list[str], beg: int, func) -> None:
     """
     [Easy] https://cses.fi/problemset/task/1622
-    [Solution] https://cses.fi/problemset/result/4776092/
+    [Solution] https://cses.fi/problemset/result/4779301/
 
     Given a string, your task is to generate all different strings that can be 
     created using its characters.
@@ -48,14 +48,14 @@ def creating_strings_inplace(inp: list[str], beg: int, func) -> None:
         func(''.join(inp))
         return
 
-    creating_strings_inplace(inp, beg + 1, func)
+    creating_strings(inp, beg + 1, func)
     for cur in range(beg + 1, len(inp)):
         inp[beg], inp[cur] = inp[cur], inp[beg]
-        creating_strings_inplace(inp, beg + 1, func)
+        creating_strings(inp, beg + 1, func)
         inp[beg], inp[cur] = inp[cur], inp[beg]
 
 
 if __name__ == '__main__':
     perms = set()
-    creating_strings_inplace(list('aabac'), 0, perms.add)
+    creating_strings(sorted('aabac'), 0, perms.add)
     print(len(perms), '\n'.join(sorted(perms)), sep='\n')
