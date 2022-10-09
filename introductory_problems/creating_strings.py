28ffabdb24bc6e90ab346f36e1d70f7000ae4676
hauntarl <sameer.mungole@gmail.com>
2022-10-09 04:50:41 -0700

    cses problem #13

---
+++ introductory_problems/creating_strings.py
@@ -0,0 +1,61 @@
+from util import timeit
+
+
+@timeit
+def creating_strings_inplace(inp: list[str], beg: int, func) -> None:
+    """
+    [Easy] https://cses.fi/problemset/task/1622
+    [Solution] https://cses.fi/problemset/result/4776092/
+
+    Given a string, your task is to generate all different strings that can be 
+    created using its characters.
+
+    The only input line has a string of length n. Each character is between a–z.
+
+    First print an integer k: the number of strings. Then print k lines: the 
+    strings in alphabetical order.
+
+    Constraints:
+    1 ≤ n ≤ 8
+
+    Input:
+    aabac
+
+    Output:
+    20
+    aaabc
+    aaacb
+    aabac
+    aabca
+    aacab
+    aacba
+    abaac
+    abaca
+    abcaa
+    acaab
+    acaba
+    acbaa
+    baaac
+    baaca
+    bacaa
+    bcaaa
+    caaab
+    caaba
+    cabaa
+    cbaaa
+    """
+    if len(inp) - beg < 2:
+        func(''.join(inp))
+        return
+
+    creating_strings_inplace(inp, beg + 1, func)
+    for cur in range(beg + 1, len(inp)):
+        inp[beg], inp[cur] = inp[cur], inp[beg]
+        creating_strings_inplace(inp, beg + 1, func)
+        inp[beg], inp[cur] = inp[cur], inp[beg]
+
+
+if __name__ == '__main__':
+    perms = set()
+    creating_strings_inplace(list('aabac'), 0, perms.add)
+    print(len(perms), '\n'.join(sorted(perms)), sep='\n')
