b5e7e9e4f28eef4665e3746311d943dfe4afcd77
hauntarl <sameer.mungole@gmail.com>
2022-11-24 04:22:52 -0700

    add bottom-up solution

--- largest_common_subsequence.py
+++ largest_common_subsequence.py
@@ -20,9 +20,27 @@ def lcs(a: str, b: str, i: int, j: int, memo: dict) -> int:
     return size
 
 
+@timeit
+def lcs_bottomup(a: str, b: str) -> int:
+    memo = [[0] * (len(b) + 1) for _ in range(len(a) + 1)]
+    for i in range(1, len(a) + 1):
+        for j in range(1, len(b) + 1):
+            if a[i - 1] == b[j - 1]:
+                memo[i][j] = memo[i - 1][j - 1] + 1
+            elif memo[i - 1][j] >= memo[i][j - 1]:
+                memo[i][j] = memo[i - 1][j]
+            else:
+                memo[i][j] = memo[i][j - 1]
+
+    return memo[-1][-1]
+
+
 if __name__ == '__main__':
     @timeit
-    def test(a: str, b: str) -> int: return lcs(a, b, 0, 0, dict())
+    def lcs_topdown(a: str, b: str) -> int: return lcs(a, b, 0, 0, dict())
 
-    test('BDCABA', 'ABCBDAB')
-    test('HIEROGLYPHOLOGY', 'MICHAELANGELO')  # HELLO - count=5
+    lcs_topdown('BDCABA', 'ABCBDAB')
+    lcs_topdown('HIEROGLYPHOLOGY', 'MICHAELANGELO')  # HELLO - count=5
+
+    lcs_bottomup('BDCABA', 'ABCBDAB')
+    lcs_bottomup('HIEROGLYPHOLOGY', 'MICHAELANGELO')  # HELLO - count=5
